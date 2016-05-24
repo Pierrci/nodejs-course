2a37d78f314cb43e8e73efb527c4cb1c9546bc3d
Pierric Cistac <pierrot007@gmail.com>
2016-05-25 01:12:23 +0200

    simple routing with express

---
+++ controller.js
@@ -0,0 +1,37 @@
+'use strict'
+
+let path = require('path') // Utilisé pour récupérer les chemins des fichiers
+
+function allInit(req, res, next) {
+  console.log("All init")
+  next() // allInit est utilisé en tant que 'middleware' et doit rendre la main une fois terminé pour que le handler suivant puisse s'exécuter
+}
+
+function allAuth(req, res, next) {
+  console.log("All auth")
+  next() // Même chose que allInit
+}
+
+function getIndex(request, response) {
+  // __dirname retourne le chemin du dossier dans lequel le script node est exécuté
+  // path.join se charge de retourné un chemin valide en concaténant correctement l'ensemble des arguments
+  response.sendFile(path.join(__dirname, 'index.html'))
+}
+
+function getForm(request, response) {
+  response.sendFile(path.join(__dirname, 'form.html'))
+}
+
+function postForm(request, response) {
+  // Express ajoute une méthode 'json' à l'objet 'response' qui permet de retourner simplement un objet JSON
+  response.json({ result: 'success '})
+}
+
+// ES6 nous permet d'éviter d'avoir un objet sous la forme { allInit: allInit, allAuth: allAuth, ... }
+module.exports = {
+  allInit,
+  allAuth,
+  getIndex,
+  getForm,
+  postForm
+}
---
+++ index.js
@@ -0,0 +1,43 @@
+'use strict'
+
+let express = require('express')
+let app = express()
+let router = express.Router()
+
+let controller = require('./controller')
+
+// Ici on utilise une nouveauté de ES6, la destructuration d'objet, c'est l'inverse de 'let controller = { allInit, allAuth, ... }'
+// https://hacks.mozilla.org/2015/05/es6-in-depth-destructuring/
+// On peut ainsi appeler les fonctions simplement, au lieu de devoir faire controller.allInit ou controller.allAuth
+let { allInit, allAuth, getIndex, getForm, postForm } = controller
+
+// On indique qu'on va utiliser le router créé
+app.use(router)
+
+/**
+ * Agit comme un middleware, puisqu'intercepte toutes les requêtes sur toutes les routes
+ * La fonction allInit appelle bien next(), ce qui permet de rendre la main au handler suivant
+ */
+router
+  .route('*')
+  .all(allInit)
+
+/**
+ * GET sur la page d'accueil
+ */
+router
+  .route('/')
+  .get(getIndex)
+
+/**
+ * Url '/form'
+ * Le GET retourne la page de saisie du formulaire
+ * Le POST traite les données envoyées via la fonction postForm
+ */
+router
+  .route('/form')
+  .get(getForm)
+  .post(postForm)
+
+// Indispensable pour mettre le serveur en écoute sur le port spécifié
+app.listen(8080)
